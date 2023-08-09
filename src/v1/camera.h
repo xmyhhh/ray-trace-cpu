e6dd35950873076d6d80daf0f6f0feb5448ec394
xmyhhh <xmycissy@outlook.com>
2023-08-09 18:03:14 +0800

    1

--- src/v1/camera.h
+++ src/v1/camera.h
@@ -25,10 +25,15 @@ void write_color(std::ostream& out, vec3 pixel_color, int samples_per_pixel) {
 class camera {
 public:
 
-	double aspect_ratio = 16.0 / 9.0;  // Ratio of image width over height
-	int    image_width = 1920 * 2;  // Rendered image width in pixel count
-	int    samples_per_pixel = 35;   // Count of random samples for each pixel
+	double aspect_ratio = 1.0;  // Ratio of image width over height
+	int    image_width = 100;  // Rendered image width in pixel count
+	int    samples_per_pixel = 10;   // Count of random samples for each pixel
 	int    max_depth = 10;   // Maximum number of ray bounces into scene
+
+	double vfov = 90;              // Vertical view angle (field of view)
+	point3 lookfrom = point3(0, 0, -1);  // Point camera is looking from
+	point3 lookat = point3(0, 0, 0);   // Point camera is looking at
+	vec3   vup = vec3(0, 1, 0);     // Camera-relative "up" direction
 
 	ray get_ray(int i, int j) const {
 		// Get a randomly sampled camera ray for the pixel at location i,j.
@@ -53,7 +58,7 @@ public:
 		initialize();
 
 		std::ofstream myfile;
-		myfile.open("v1example_lamber.ppm", std::ios::out | std::ios::binary | std::ios::trunc);
+		myfile.open("v1example_lamber_re_f.ppm", std::ios::out | std::ios::binary | std::ios::trunc);
 
 		myfile << "P3\n" << image_width << " " << image_height << "\n255\n";
 		std::vector<color> image(image_width * image_height);
@@ -84,29 +89,34 @@ private:
 	point3 pixel00_loc;    // Location of pixel 0, 0
 	vec3   pixel_delta_u;  // Offset to pixel to the right
 	vec3   pixel_delta_v;  // Offset to pixel below
+	vec3   u, v, w;        // Camera frame basis vectors
 
 	void initialize() {
 		image_height = static_cast<int>(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
 
-		center = point3(0, 0, 0);
+		center = lookfrom;
 
 		// Determine viewport dimensions.
-		auto focal_length = 1.0;
+		auto focal_length = (lookfrom - lookat).length();
 		auto viewport_height = 2.0;
 		auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
+		// Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
 		// Calculate the vectors across the horizontal and down the vertical viewport edges.
-		auto viewport_u = vec3(viewport_width, 0, 0);
-		auto viewport_v = vec3(0, -viewport_height, 0);
+		vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
+		vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
 
 		// Calculate the horizontal and vertical delta vectors from pixel to pixel.
 		pixel_delta_u = viewport_u / image_width;
 		pixel_delta_v = viewport_v / image_height;
 
 		// Calculate the location of the upper left pixel.
-		auto viewport_upper_left =
-			center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+		auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 	}
 
